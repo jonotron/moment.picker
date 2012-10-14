81b29c3dc41bfac1ff7e7a01a70042c90822a931
Jonathan Bowers <jonotron@gmail.com>
2012-10-14 12:15:34 -0700

    Tests for instantiation with a date option

--- test/spec/moment.picker.spec.js
+++ test/spec/moment.picker.spec.js
@@ -1,13 +1,19 @@
 describe('moment.picker', function() {
   
   describe('when instantiating a new picker', function() {
-    var picker;
+    var picker; // full suite available momentpicker
     beforeEach(function() {
-      picker = true; 
+      picker = new MomentPicker(); 
     });
-    it('should pass some tests', function() {
-      
-      expect(picker).toBeEqual(true); 
+
+    it('should have a default selected day of today', function() {
+      expect(picker.getSelectedMoment()).toEqual(moment().sod()); 
     });
+
+    it('should select a different day, if a moment is provided in the options', function() {
+      var picker = new MomentPicker({date: moment('Jan 1, 2000')});
+      expect(picker.getSelectedMoment()).toEqual(moment('Jan 1, 2000').sod()); 
+    });
+
   });
 });
